620bd206cca59adfeb407962475829cb50eb99ba
catmoska <84236986+catmoska@users.noreply.github.com>
2022-06-09 20:48:25 +0300

    init 18

--- CryptoRunner/models.py
+++ CryptoRunner/models.py
@@ -58,7 +58,19 @@ class Boks(models.Model):
 
 
 
-
+"""
+подготовка бази даних 
+1 создания бд
+    python manage.py makemigrations CryptoRunner
+    python manage.py migrate
+2 создания админки
+    python manage.py createsuperuser
+3 создания NFT
+    нузна создать хоть одно nft
+4 создания Box
+    нузна создать одно Box (другий бокси не как не роботают)
+всо
+"""
 
 
 # adminGj
